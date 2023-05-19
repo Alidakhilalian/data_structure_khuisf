84218398443cb0a40a6a09f9a076b3fa2dbb40bb
AliDakhilalin <67735066+Alidakhilalian@users.noreply.github.com>
2023-05-19 13:07:17 +0330

    final Dijkstra

--- dijkstra/code.cpp
+++ dijkstra/code.cpp
@@ -1,72 +1,78 @@
-#include <iostream>
+#include <iostream> 
+
+#define V 9
+
+int graph[V][V] = {
+    {0, 4, 0, 0, 0, 0, 0, 8, 0},
+    {4, 0, 8, 0, 0, 0, 0, 11, 0},
+    {0, 8, 0, 7, 0, 4, 0, 0, 2},
+    {0, 0, 7, 0, 9, 14, 0, 0, 0},
+    {0, 0, 0, 9, 0, 10, 0, 0, 0},
+    {0, 0, 4, 14, 10, 0, 2, 0, 0},
+    {0, 0, 0, 0, 0, 2, 0, 1, 6},
+    {8, 11, 0, 0, 0, 0, 1, 0, 7},
+    {0, 0, 2, 0, 0, 0, 6, 7, 0}
+};
 
 using namespace std;
 
-#define N 5
+int minDistance(int dist[], bool sptSet[])
+{
+    int min = INT_MAX;
+    int min_index;
 
-int main_graph[N][N] = {
-	{0,4,5,0,0},
-	{0,0,0,0,0},
-	{0,0,0,0,10},
-	{0,0,0,0,0},
-	{0,0,0,0,0},
-};
+    for (int i = 0; i < V; i++) {
+        if (sptSet[i] == false && dist[i] <= min) {
+            min = dist[i];
+            min_index = i;
+        }
+    }   
 
-
-bool check(int index) {
-	bool check_node = false;
-	for (int i = 0; i < N; i++)
-	{
-		if (index == (N - 1)) {
-			check_node = true;
-		}
-		else
-		{
-			if (main_graph[index][i] != 0) {
-				check_node = check(i);
-			}
-		}
-	}
-	return check_node;
+    return min_index;
 }
 
-int minDistance(int graph[]) {
-	int min_index = 0;
-	int min = 1000;
-	for (int i = 0; i < N; i++)
-	{
-		if (graph[i] != 0) {
-			if (graph[i] < min && check(i)) {
-				min_index = i;
-				min = graph[i];
-			}
-		}
-	}
+int printSolution(int dist[], int n)
+{
+    for (int i = 0; i < V; i++) {
+        cout << "[" << i << "] -> " << dist[i] << endl;
+    }
 
-	return min_index;
+    return 0;
 }
 
+void dijkstra(int src)
+{
+    int dist[V]; 
 
-void dijkstra(int graph[N][N]) {
+    bool sptSet[V];
 
-	int min = 0;
-	int total = 0;
-	int last = 0;
-	for (int i = 0; i < N; i++)
-	{
-		cout << min + 1;
-		last = min;
-		min = minDistance(graph[min]);
-		if (min == 0) {
-			break;
-		}
-		total += graph[last][min];
-	}
-	cout << "\nTotal Cost: " << total;
+    for (int i = 0; i < V; i++) {
+        dist[i] = INT_MAX;
+        sptSet[i] = false;
+    }
+        
+    dist[src] = 0;
+
+    for (int i = 0; i < V - 1; i++) {
+        
+        int u = minDistance(dist, sptSet);
+
+        sptSet[u] = true;
+
+        for (int j = 0; j < V; j++) {
+            
+            if (!sptSet[j] && graph[u][j] && dist[u] != INT_MAX && dist[u] + graph[u][j] < dist[j]) {
+                dist[j] = dist[u] + graph[u][j];
+            }
+        }
+    }
+
+    printSolution(dist, V);
 }
 
+int main()
+{
+    dijkstra(0);
 
-int main() {
-	dijkstra(main_graph);
+    return 0;
 }
-
